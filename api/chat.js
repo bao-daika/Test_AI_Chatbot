d8eaeee9873ceb3979a1595e0c5cf14d86e92094
bao-daika <56355940+bao-daika@users.noreply.github.com>
2026-03-09 15:33:16 -0400

    Add files via upload

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-import { petraKnowledge } from '../PetraKnowledge.js';
+import { petraKnowledge } from './PetraKnowledge.js';
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ reply: "Access Denied" });
